8ae40cbaf6ffc22f6c66e55451b9cf9867107a07
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-09 21:12:48 +0800

    任明德 20210335    Static hostname: MonstrousMooshine          Icon name: computer-laptop            Chassis: laptop         Machine ID: cbe16825cc74483484c518d8358fa0ff            Boot ID: 5f19ff35b7e54cd583f998b3d3b8cf2a   Operating System: Deepin 20.2.1             Kernel: Linux 5.10.29-amd64-desktop       Architecture: x86-64  21:12:48 up 48 min,  1 user,  load average: 0.91, 0.97, 1.01

--- examples/counter/top.v
+++ examples/counter/top.v
@@ -5,8 +5,7 @@ module top (
     output reg [3:0] out);
     
     always @(posedge clk) begin
-        // out <= reset ? 0 : out + 1;
-        out <= out + 1;
+        out <= out - 1;
     end
     
 endmodule
